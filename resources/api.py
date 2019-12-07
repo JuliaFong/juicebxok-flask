4237657e4164750c612810d7bf3e5598499fedf7
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-12-06 16:04:02 -0800

    adds api fixes

--- resources/api.py
+++ resources/api.py
@@ -2,11 +2,12 @@ from flask import request, jsonify, Blueprint
 from playhouse.shortcuts import model_to_dict
 from yelp.client import Client
 
+import config
+
 api = Blueprint('apis', 'api')
-client = Client(process.env.API_KEY)
+client = Client(config.API_KEY)
 
 
 @api.route('/', methods=["POST"])
 def get_locations():
-    print(process.env.API_KEY)
-    
+        
